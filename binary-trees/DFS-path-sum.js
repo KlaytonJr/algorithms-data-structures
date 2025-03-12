115d0c7e209091dc0f4621b0f231c17d817b5fd5
klayton Junior <klayton.junior@iaris.com>
2025-03-12 20:19:26 -0300

    feat: resolve path sum binary tree DFS challenge 112 LeetCode

---
+++ binary-trees/DFS-path-sum.js
@@ -0,0 +1,10 @@
+function hasPathSum(root, targetSum) {
+  if (!root) return false;
+
+  if (!root.left && !root.right && targetSum - root.val === 0) return true;
+
+  return (
+    hasPathSum(root.left, targetSum - root.val) ||
+    hasPathSum(root.right, targetSum - root.val)
+  );
+}
